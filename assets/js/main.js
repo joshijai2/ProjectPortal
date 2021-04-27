a544037d7c4862fbbdd2120d6ed9ffdbbe1c196f
joshijai2 <joshi.jai232@gmail.com>
2021-04-27 11:58:58 +0530

    add dashboard
    add new page
    removed extra files and scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,28 +18,28 @@ $('.facultyLogin').click(function (e) {
 // Close button
 $('.closeButton').on('click', function () {
   $('body').css('overflow', 'auto');
-  if($('.student.popup').css('display')!='none')
+  if ($('.student.popup').css('display') != 'none')
     $('.student.popup').fadeOut();
-  else if($('.faculty.popup').css('display')!='none')
+  else if ($('.faculty.popup').css('display') != 'none')
     $('.faculty.popup').fadeOut();
   $('.pageOverlay').fadeOut();
 });
 
-//Close pn click outside
-$('body').click(function() {
+// Close on click outside
+$('body').click(function () {
   $('body').css('overflow', 'auto');
-  if($('.student.popup').css('display')!='none')
+  if ($('.student.popup').css('display') != 'none')
     $('.student.popup').fadeOut();
-  else if($('.faculty.popup').css('display')!='none')
+  else if ($('.faculty.popup').css('display') != 'none')
     $('.faculty.popup').fadeOut();
   $('.pageOverlay').fadeOut();
 });
 
-$('.popup').click(function(e) {
+$('.popup').click(function (e) {
   e.stopPropagation();
 });
 
-// transitions
+// login signup transitions---------------------------------------------
 const studentSignUp = document.getElementById('studentSignUp');
 const studentSignIn = document.getElementById('studentSignIn');
 const studentContainer = document.getElementById('studentContainer');
@@ -188,7 +188,7 @@ facultySignIn.addEventListener('click', () => {
   }, true)
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
+   * Scroll with ofset on links with a class name .scrollto
    */
   on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
@@ -212,122 +212,6 @@ facultySignIn.addEventListener('click', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
         scrollto(window.location.hash)
-      }
-    }
-  });
-
-  /**
-   * Clients Slider
-   */
-  new Swiper('.clients-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120
-      }
-    }
-  });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function (e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function (el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        aos_init();
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfokio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40
-      },
-
-      1200: {
-        slidesPerView: 3,
       }
     }
   });
