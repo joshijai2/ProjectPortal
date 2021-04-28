78f72c55615035c3577956bcfb368c7af656dbe2
joshijai2 <joshi.jai232@gmail.com>
2021-04-28 13:52:31 +0530

    login signup connected with backend

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,54 +1,151 @@
 // Faculty Signup
 // -----------------------------------------------------------
-
-function facultySignup() {
+$("#facSignupBtn").on("click", function () {
     var data = JSON.stringify({
-        "name": document.facultySignup.name,
-        "uid": document.facultySignup.uid,
-        "email": document.facultySignup.email,
-        "password": document.facultySignup.pass
+        "name": document.facultySignup.name.value,
+        "uid": document.facultySignup.uid.value,
+        "email": document.facultySignup.email.value,
+        "password": document.facultySignup.pass.value
     });
+
     console.log(data);
-    // var xhr = new XMLHttpRequest();
-    // xhr.withCredentials = true;
 
-    // xhr.addEventListener("readystatechange", function () {
-    //     if (this.readyState === 4) {
-    //         console.log(this.responseText);
-    //     }
-    // });
-    // xhr.open("POST", "https://projenarator.herokuapp.com/register-faculty/");
-    // xhr.setRequestHeader("Content-Type", "application/json");
+    var xhr = new XMLHttpRequest();
 
-    // xhr.send(data);
-    // xhr.onload = () => { console.log(this.responseText) };
-    // console.log(this.status);
-};
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log('this.responseText :>> ', this.responseText);
+            console.log('this.status :>> ', this.status);
+
+            if (this.status === 0 || (this.status >= 200 && this.status < 400)) {
+                // The request has been completed successfully
+
+                var data = JSON.parse(this.responseText)
+                localStorage.setItem("Token", "Token " + data.token)
+                localStorage.setItem("name", data.name)
+                localStorage.setItem("email", data.email)
+                localStorage.setItem("uid", data.uid)
+                window.location.replace('dashboard.html')
+            }
+        }
+    });
+
+    xhr.open("POST", "https://projenarator.herokuapp.com/register-faculty/");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
+
+
+});
 
 // Faculty Signin
 // -----------------------------------------------------------
+$("#facSigninBtn").on("click", function () {
+    var data = JSON.stringify({
+        "uid": document.facultySignin.uid.value,
+        "password": document.facultySignin.pass.value
+    });
 
-function facultySignin() {
-    console.log("fac signin")
-};
+    console.log(data);
 
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log('this.responseText :>> ', this.responseText);
+            console.log('this.status :>> ', this.status);
+
+            if (this.status === 0 || (this.status >= 200 && this.status < 400)) {
+                // The request has been completed successfully
+
+                var data = JSON.parse(this.responseText)
+                localStorage.setItem("Token", "Token " + data.token)
+                localStorage.setItem("name", data.name)
+                localStorage.setItem("email", data.email)
+                localStorage.setItem("uid", data.uid)
+                window.location.replace('dashboard.html')
+            }
+        }
+    });
+
+    xhr.open("POST", "https://projenarator.herokuapp.com/login/");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
+});
 
 // Student Signup
 // -----------------------------------------------------------
 
-function studentSignup() {
-    var data = {
-        "name": document.studentSignup.name,
-        "uid": document.studentSignup.uid,
-        "email": document.studentSignup.email,
-        "password": document.studentSignup.pass
-    }
-};
+$("#stuSignupBtn").on("click", function () {
+    var data = JSON.stringify({
+        "name": document.studentSignup.name.value,
+        "uid": document.studentSignup.uid.value,
+        "email": document.studentSignup.email.value,
+        "password": document.studentSignup.pass.value
+    });
 
+    console.log(data);
 
-// Student Signup
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log('this.responseText :>> ', this.responseText);
+            console.log('this.status :>> ', this.status);
+
+            if (this.status === 0 || (this.status >= 200 && this.status < 400)) {
+                // The request has been completed successfully
+
+                var data = JSON.parse(this.responseText)
+                localStorage.setItem("Token", "Token " + data.token)
+                localStorage.setItem("name", data.name)
+                localStorage.setItem("email", data.email)
+                localStorage.setItem("uid", data.uid)
+                window.location.replace('dashboard.html')
+            }
+        }
+    });
+
+    xhr.open("POST", "https://projenarator.herokuapp.com/register-student/");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
+});
+
+// Student Signin
 // -----------------------------------------------------------
 
-function studentSignin() {
-    console.log("stu signin")
-};
+$("#stuSigninBtn").on("click", function () {
+    var data = JSON.stringify({
+        "uid": document.studentSignin.uid.value,
+        "password": document.studentSignin.pass.value
+    });
+
+    console.log(data);
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log('this.responseText :>> ', this.responseText);
+            console.log('this.status :>> ', this.status);
+
+            if (this.status === 0 || (this.status >= 200 && this.status < 400)) {
+                // The request has been completed successfully
+
+                var data = JSON.parse(this.responseText)
+                localStorage.setItem("Token", "Token " + data.token)
+                localStorage.setItem("name", data.name)
+                localStorage.setItem("email", data.email)
+                localStorage.setItem("uid", data.uid)
+                window.location.replace('dashboard.html')
+            }
+        }
+    });
+
+    xhr.open("POST", "https://projenarator.herokuapp.com/login/");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.send(data);
+});
