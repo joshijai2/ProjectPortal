ccae2e8251ba134c5e60b45091412f767105b4df
HRITISHA <hritishak@gmail.com>
2021-05-22 18:50:35 +0530

    addprj

--- assets/js/add_project.js
+++ assets/js/add_project.js
@@ -4,16 +4,14 @@ $("#addprj").on("click", function () {
         "title": document.addnew.text.value,
         "start_date": document.addnew.sdate.value,
         "end_date": document.addnew.fdate.value,
-        "file": document.addnew.file.value,
-        "link": document.addnew.link.value,
-        "author": " {\"author\" : [{\"name\" : \"" + document.addnew.sname.value + "\",\"regno\" : \"" + document.addnew.rno.value + "\"}]}",
-        "faculty": document.addnew.faculty.value,
-
-        // "file", fileInput.files[0], "VL2019205003491_DA.pdf");
-        "course_code": document.addnew.code.value,
-        "course_name": document.addnew.cname.value,
-        "duration": document.addnew.duration.value,
-        "description": document.addnew.desc.value,
+        "file":  document.addnew.file.value,
+        "link" : document.addnew.link.value,
+        "author" :" {\"author\" : [{\"name\" : \""+document.addnew.sname.value+"\",\"regno\" : \""+document.addnew.rno.value+"\"}]}",
+        "faculty" : document.addnew.faculty.value,
+        "course_code" :document.addnew.code.value,
+        "course_name" :document.addnew.cname.value,
+        "duration" :document.addnew.duration.value,
+        "description" :document.addnew.desc.value,
         // "domain" :document.addnew.domain.value,
         // "facultyid" :document.addnew.fid.value,
 
@@ -21,45 +19,17 @@ $("#addprj").on("click", function () {
     console.log(data);
 
     let error = "";
-    let name = /^[A-Za-z0-9]$/;
+    let name = /^[A-Za-z]$/;
     let regno = /^[0-9]{2}[A-Z]{3}[0-9]{4}$/;
-    let letters = /^[A-Za-z]$/;
-    let urllink = /^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/;
-    let coursecode = /^[A-Z]{3}[0-9]{4}$/;
-    let duration = /^[0-9]$/;
 
     let flag = 0
 
-    if (!name.test(data['text'])) {
-        error += ">> Enter proper name of Project! (No Special Characters)\n";
+    if (!uid.test(data['uid'])) {
+        error += ">> Enter correct format of Reg No.!\n";
         flag = 1;
     }
-    if (!regno.test(data['rno'])) {
-        error += ">> Registration Number should be of the type 11XXX1111!\n";
-        flag = 1;
-    }
-    if (!letters.test(data['sname'])) {
-        error += ">> Student Name should consist of Alphabets!\n";
-        flag = 1;
-    }
-    if (!letters.test(data['faculty'])) {
-        error += ">> Faculty Name should consist of !\n";
-        flag = 1;
-    }
-    if (!urllink.test(data['link'])) {
-        error += ">> Link should be of proper format!\n";
-        flag = 1;
-    }
-    if (!coursecode.test(data['code'])) {
-        error += ">> Course Code should be of the format XXX1111!\n";
-        flag = 1;
-    }
-    if (!letters.test(data['cname'])) {
-        error += ">> Course Name should not contain any special characters or Numbers!\n";
-        flag = 1;
-    }
-    if (!duration.test(data['duration'])) {
-        error += ">> Duration should contain numbers (in months)!\n";
+    if (!password.test(data['password'])) {
+        error += ">> Password must be of atleast length 8!\n";
         flag = 1;
     }
     if (flag) {
@@ -77,13 +47,14 @@ $("#addprj").on("click", function () {
                 // The request has been completed successfully
                 var data = JSON.parse(this.responseText)
                 sessionStorage.setItem("prj_add", 1);
-
-                window.location.replace('dashboard.html')
+                
+                window.location.replace('dashboard.html');      
             } else {
                 try {
                     var data = JSON.parse(this.responseText);
                     alert(Object.values(data)[0][0]);
-                } catch (err) {
+                }
+                catch (err) {
                     alert("Error in adding project");
                 }
             }
