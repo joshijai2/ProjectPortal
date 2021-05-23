e4cf3de39488ef8444de5bb62adbe8380bb7eff3
HRITISHA <hritishak@gmail.com>
2021-05-23 22:29:16 +0530

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -26,7 +26,7 @@ function display(projects, page = 1) {
       
       $("#row" + row).append(
         `
-        <div class="col-lg-4 d-flex flex-column justify-content-center">
+        <div class="col-lg-4 d-flex flex-column justify-content-center" padding="5%">
           <div id="` + i + `" class="card bg-card" onclick="viewProject(` + projects[i]["uuid"] + `)" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">`+ projects[i]["title"] + `</h5>
